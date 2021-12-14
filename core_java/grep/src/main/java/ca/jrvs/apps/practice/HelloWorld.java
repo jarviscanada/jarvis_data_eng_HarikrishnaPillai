2087290ae891acf79dd12fc0ced8d0335b5537c7
Harikrishna Pillai <harix.pix5@gmail.com>
2021-12-14 20:42:59 +0000

    Java_Regex

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -3,8 +3,9 @@ class HelloWorld {
 
     // Your program begins with a call to main().
     //   // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
+    public static void main(String arg[]) {
+        RegexExc regex = new RegexExcImp();
+        String intext = "name.jpeg";
+        System.out.println(regex.matchJpeg(intext));
     }
 }
--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,5 +1,23 @@
 package ca.jrvs.apps.practice;
+import java.util.Scanner;
 
-public class RegexExcImp {
-    
+public class RegexExcImp implements RegexExc {
+
+
+
+    @Override
+    public boolean matchJpeg(String filename) {
+        return filename.matches("([^\\s]+(\\.(?i)(jpe?g))$)");
+    }
+
+    @Override
+    public boolean matchIp(String ip) {
+        return ip.matches("\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}");
+    }
+
+    @Override
+    public boolean isEmptyLine(String line) {
+        return line.matches("^\\s*$");
+    }
 }
+
