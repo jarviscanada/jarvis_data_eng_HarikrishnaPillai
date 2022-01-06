5648bb450eb0f5be997068c56984b75df780b7f7
Harikrishna Pillai <harix.pix5@gmail.com>
2022-01-06 22:28:05 +0000

    Implemented Docker Updated Lambda, Uber Jar

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,130 +1,116 @@
 package ca.jrvs.apps.grep;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 
 public class JavaGrepImp implements JavaGrep {
 
   final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
-
   private String regex;
   private String rootPath;
   private String outFile;
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     if (args.length != 3) {
-      throw new IllegalArgumentException("Wrong Syntax! USE: JavaGrep regex rootPath outFile");
+
+      throw new IllegalArgumentException("Usage: JavaGrep regex rootPath outFile");
     }
-
-    // Using default Logger configuration
     BasicConfigurator.configure();
-    JavaGrepImp GrepImp = new JavaGrepImp();
 
-    // Store the arguments using setters
-    GrepImp.setRegex(args[0]);
-    GrepImp.setRootPath(args[1]);
-    GrepImp.setOutFile(args[2]);
+    JavaGrepImp javaGrepImp = new JavaGrepImp();
+    javaGrepImp.setRegex(args[0]);
+    javaGrepImp.setRootPath(args[1]);
+    javaGrepImp.setOutFile(args[2]);
+    try {
+      javaGrepImp.process();
+    } catch (Exception ex) {
 
-    GrepImp.process();
+      javaGrepImp.logger.error("Main process Failed", ex);
+    }
   }
 
   @Override
-  public void process() throws IOException { // Top level workflow
-    List<String> MatchedLines = new ArrayList<String>();
-    for (File file : listFiles(rootPath)) {
-      for (String temp : readLines(file))
-        if (containsPattern(temp)) {
-          MatchedLines.add(temp);
+  public void process() throws IOException {
+    List<String> lines = new ArrayList<>();
+    for (File newFiles : listFiles(getRootPath())) {
+      for (String line : readLines(newFiles)) {
+        if (containsPattern(line)) {
+          lines.add(line);
         }
-      if (MatchedLines.size() != 0) {
-        try {
-          this.writeToFile(MatchedLines);
-        } catch (IOException e) {
-          throw new IOException("Writing was not Executed");
-        }
-      } else {
-        logger.warn("Try again with another String");
       }
     }
+    writeToFile(lines);
   }
 
   @Override
   public List<File> listFiles(String rootDir) {
-    try {
-      File f = new File(rootDir);
-      ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
-      return files;
-    } catch (IllegalArgumentException ex) {
-      System.out.println("Argument problem"); // Exception
-      return null;
+    File dirPath = new File(rootDir);
+    File[] fileList = dirPath.listFiles();
+    if (fileList == null) {
+      try {
+        throw new FileNotFoundException("ERROR: Root Path is Empty.");
+      } catch (FileNotFoundException e) {
+        logger.error("FileNotFoundException", e);
+      }
     }
+    List<File> result = new ArrayList<>();
+    for (File file : fileList)
+      if (file.isFile()) {
+        result.add(file);
+      } else {
+        String subDirName = file.getAbsolutePath();
+        /*collect all files in the directory. */
+        result.addAll(listFiles(subDirName));
+      }
+
+    return result;
   }
 
-  @Override
-  public List<String> readLines(File inputFile) {
-    List<String> Lines = new ArrayList<String>();
-    BufferedReader reader;
+  public List<String> readLines(File inputFile) throws FileNotFoundException {
+    List<String> lineList = new ArrayList<>();
     try {
-      reader = new BufferedReader(new FileReader(inputFile));
-      String line = reader.readLine();
-      while (line != null) {
-        Lines.add(line);
-        line = reader.readLine();
-      }
-      reader.close();
-    } catch (IOException e) {
-      e.printStackTrace(); //
-    }
 
-    return Lines;
+      BufferedReader buffReader =
+          new BufferedReader(
+              new FileReader(inputFile)); // creates a buffering character input stream
+      String line;
+      while ((line = buffReader.readLine()) != null) {
+        lineList.add(line);
+      }
+      buffReader.close();
+
+    } catch (IOException e) {
+
+      logger.error("InputOutput Exception", e);
+    }
+    return lineList;
   }
 
   @Override
   public boolean containsPattern(String line) {
-    return line.matches(this.regex);
+    return line.matches(getRegex());
   }
 
   @Override
   public void writeToFile(List<String> lines) throws IOException {
-    File fileOut = new File(this.outFile);
-    FileOutputStream fos = new FileOutputStream(fileOut);
-
-    BufferedWriter BuffWrite = new BufferedWriter(new OutputStreamWriter(fos));
-    for (String temp: lines) {
-          try {
-            BuffWrite.write(temp);
-            BuffWrite.newLine();
-          }catch (IOException e) {
-            e.printStackTrace();
-          }
-    } BuffWrite.close();
+    BufferedWriter writer = new BufferedWriter(new FileWriter(getOutFile()));
+    for (String eachLine : lines) {
+      writer.write(eachLine + System.lineSeparator());
+    }
+    writer.close();
   }
 
-  @Override
   public String getRegex() {
     return regex;
   }
 
-  @Override
   public void setRegex(String regex) {
     this.regex = regex;
-  }
-
-  @Override
-  public String getRootPath() {
-    return rootPath;
-  }
-
-  @Override
-  public void setRootPath(String rootPath) {
-    this.rootPath = rootPath;
   }
 
   @Override
@@ -132,9 +118,15 @@ public class JavaGrepImp implements JavaGrep {
     return outFile;
   }
 
-  @Override
+  public String getRootPath() {
+    return rootPath;
+  }
+
+  public void setRootPath(String rootPath) {
+    this.rootPath = rootPath;
+  }
+
   public void setOutFile(String outFile) {
     this.outFile = outFile;
   }
 }
-
