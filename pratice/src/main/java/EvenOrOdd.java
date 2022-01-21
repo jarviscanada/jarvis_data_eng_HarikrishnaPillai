050f1202b9dd21f1b3c7ec02f927b2b28655aa72
Harikrishna Pillai <harix.pix5@gmail.com>
2022-01-21 15:27:55 +0000

    EvenOrOdd

---
+++ pratice/src/main/java/EvenOrOdd.java
@@ -0,0 +1,8 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println(EvenOrOdd(5));
+    }
+   public static boolean EvenOrOdd(int a){
+       return a%2 == 0;
+   }
+}
